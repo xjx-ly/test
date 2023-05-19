b67efd8db79088d0a8edf2fb947908731133793d
xjx-ly <134012024+xjx-ly@users.noreply.github.com>
2023-05-19 15:04:30 +0800

    Update py.py

--- Streamlit/py.py
+++ Streamlit/py.py
@@ -313,8 +313,8 @@ def Line():
     
 @st.cache(allow_output_mutation=True)
 def load_data():
-    d1 =pd.read_csv(path1,encoding='gbk')
-    d2 =pd.read_csv(path2,encoding='gbk')
+    d1 =pd.read_csv(path1,encoding=‘UTF-8’)
+    d2 =pd.read_csv(path2,encoding=‘UTF-8’)
     df=pd.concat([d1,d2])   #合并数据
     df = df.reset_index(drop=True)
 
